588dbd39d4e45905cfd5d45af21339355e16c81d
Chris Webb <chris@arachsys.com>
2025-04-28 10:55:06 +0100

    Use select() instead of poll() to work around macOS bugs
    On macOS, poll() does not work on character devices, including serial
    interfaces and pseudo-terminals. This bug has languished for quarter of a
    century, despite the legacy select() interface not being similarly broken.
    
    Competence is unlikely to break out in Apple's kernel team, so adapt
    a version of ucom to use select() to work around this mess. On better
    operating systems, you probably want the mainline branch of ucom instead.

--- ucom.c
+++ ucom.c
@@ -2,16 +2,16 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
-#include <poll.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/select.h>
 #include <termios.h>
 #include <unistd.h>
 
-static int events[2];
-static struct pollfd fd[3];
+static int events[2], fd[3], limit;
 static struct termios saved[2];
+static fd_set rfd, wfd;
 
 static size_t head[2], tail[2];
 static char data[2][PIPE_BUF];
@@ -23,13 +23,13 @@ static void quit(int signal) {
 
 static void restore(void) {
   for (int i = 0; i < 2; i++)
-    tcsetattr(fd[i].fd, TCSADRAIN, saved + i);
-  write(fd[0].fd, "\r", 1);
+    tcsetattr(fd[i], TCSADRAIN, saved + i);
+  write(fd[0], "\r", 1);
 }
 
 static void setup(void) {
   for (int i = 0; i < 2; i++)
-    if (tcgetattr(fd[i].fd, saved + i) < 0)
+    if (tcgetattr(fd[i], saved + i) < 0)
       err(1, "tcgetattr");
 
   for (int i = 0; i < 2; i++) {
@@ -46,40 +46,32 @@ static void setup(void) {
       termios.c_cc[VSUSP] = _POSIX_VDISABLE;
     }
 
-    if (tcsetattr(fd[i].fd, TCSAFLUSH, &termios) < 0)
+    if (tcsetattr(fd[i], TCSAFLUSH, &termios) < 0)
       restore(), err(1, "tcsetattr");
   }
 }
 
 static int get(int src, int dst) {
-  ssize_t count = read(fd[src].fd, data[src] + head[src],
+  ssize_t count = read(fd[src], data[src] + head[src],
     sizeof data[src] - head[src]);
 
   if (count < 0)
     return errno != EAGAIN && errno != EINTR;
   head[src] += count;
 
-  if (head[src] >= sizeof data[src])
-    fd[src].events &= ~POLLIN;
-  if (head[src] > tail[dst])
-    fd[dst].events |= POLLOUT;
-
   return count == 0;
 }
 
 static int put(int src, int dst) {
-  ssize_t count = write(fd[dst].fd, data[src] + tail[dst],
+  ssize_t count = write(fd[dst], data[src] + tail[dst],
     head[src] - tail[dst]);
 
   if (count < 0)
     return errno != EAGAIN && errno != EINTR;
   tail[dst] += count;
 
-  if (tail[dst] >= head[src]) {
+  if (tail[dst] >= head[src])
     head[src] = tail[dst] = 0;
-    fd[src].events |= POLLIN;
-    fd[dst].events &= ~POLLOUT;
-  }
 
   return 0;
 }
@@ -90,24 +82,26 @@ int main(int argc, char **argv) {
     return 64;
   }
 
-  fd[0].fd = open("/dev/tty", O_RDWR | O_NOCTTY | O_NONBLOCK);
-  if (fd[0].fd < 0)
+  fd[0] = open("/dev/tty", O_RDWR | O_NOCTTY | O_NONBLOCK);
+  if (fd[0] < 0)
     err(1, "open /dev/tty");
-  if (!isatty(fd[0].fd))
+  if (!isatty(fd[0]))
     errx(1, "/dev/tty is not a tty");
-  fd[0].events = POLLIN;
 
-  fd[1].fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
-  if (fd[1].fd < 0)
+  fd[1] = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
+  if (fd[1] < 0)
     err(1, "open %s", argv[1]);
-  if (!isatty(fd[1].fd))
+  if (!isatty(fd[1]))
     errx(1, "%s is not a tty", argv[1]);
-  fd[1].events = POLLIN;
 
   if (pipe(events) < 0)
     err(1, "pipe");
-  fd[2].fd = events[0];
-  fd[2].events = POLLIN;
+  fd[2] = events[0];
+
+  limit = fd[0] < fd[1] ? fd[1] : fd[0];
+  limit = limit < fd[2] ? fd[2] : limit;
+  if (limit >= FD_SETSIZE)
+    errx(1, "select() is limited to %u file descriptors", FD_SETSIZE);
 
   signal(SIGHUP, quit);
   signal(SIGINT, quit);
@@ -116,27 +110,34 @@ int main(int argc, char **argv) {
 
   setup();
 
-  while (fd[2].revents == 0) {
-    if (poll(fd, sizeof fd / sizeof *fd, -1) < 0) {
+  do {
+    FD_ZERO(&rfd);
+    FD_ZERO(&wfd);
+    FD_SET(fd[2], &rfd);
+
+    for (int i = 0; i < 2; i++) {
+      if (head[i] < sizeof data[i])
+        FD_SET(fd[i], &rfd);
+      if (tail[i] < head[1 - i])
+        FD_SET(fd[i], &wfd);
+    }
+
+    if (select(limit + 1, &rfd, &wfd, NULL, NULL) < 0) {
       if (errno != EAGAIN && errno != EINTR)
-        restore(), err(1, "poll");
+        restore(), err(1, "select");
       continue;
     }
 
-    for (int i = 0; i < 3; i++)
-      if (fd[i].revents & (POLLERR | POLLNVAL))
-        restore(), errx(1, "Failed to poll fd %d", fd[i].fd);
-
-    if (fd[0].revents & (POLLIN | POLLHUP) && get(0, 1))
+    if (FD_ISSET(fd[0], &rfd) && get(0, 1))
       restore(), errx(1, "Failed to read from /dev/tty");
-    if (fd[1].revents & (POLLIN | POLLHUP) && get(1, 0))
+    if (FD_ISSET(fd[1], &rfd) && get(1, 0))
       restore(), errx(1, "Failed to read from %s", argv[1]);
 
-    if (fd[0].revents & POLLOUT && put(1, 0))
+    if (FD_ISSET(fd[0], &wfd) && put(1, 0))
       restore(), errx(1, "Failed to write to /dev/tty");
-    if (fd[1].revents & POLLOUT && put(0, 1))
+    if (FD_ISSET(fd[1], &wfd) && put(0, 1))
       restore(), errx(1, "Failed to write to %s", argv[1]);
-  }
+  } while (!FD_ISSET(fd[2], &rfd));
 
   restore();
   warnx("%s closed", argv[1]);
